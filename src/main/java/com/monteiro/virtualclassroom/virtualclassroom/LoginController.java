cbcf8d81b982ffe05749ef288303532b291947ac
SilviaLobbova <silvi.lobbova@gmail.com>
2020-01-27 12:33:46 +0100

    Empty classroomList case

--- src/main/java/com/monteiro/virtualclassroom/virtualclassroom/LoginController.java
+++ src/main/java/com/monteiro/virtualclassroom/virtualclassroom/LoginController.java
@@ -34,47 +34,52 @@ public class LoginController{
             @RequestParam String login_name,
             @RequestParam String login_password,
             //we are also sending back the model from the DispatcherServlet controller to the view
-            User user,
             HttpSession session,
             Model model) throws IOException, SQLException {
 
         System.out.println("POST /LoginPage (LoginController)");
         //System.out.println(user_email);
         User myMail = UserDao.getUser(login_name, login_password);
-        System.out.println(myMail);
-        long id_classroom = (long) session.getAttribute("classroomID");
-        System.out.println(id_classroom);
+
         if(myMail == null){
             model.addAttribute("invalidCredentials", true);
             return "LoginPage";
         }
-        else if((myMail.getIsAdmin() == true)||(id_classroom == 0)){
+        else if((myMail.getIsAdmin() == true)){
             if (myMail.getUser_email().equals(login_name) &&
                     myMail.getUser_password().equals(login_password)){
                 addUserInSession(myMail,session);
+                System.out.println("empty classroomList or admin - admin post method");
+                return "redirect:/adminConnected";
             }
-            System.out.println("empty classroomList - admin post method");
-            System.out.println("render admin post method");
-            return "redirect:/adminConnected";
         }
         else if(myMail.getIsAdmin()==false){
-            if (myMail.getUser_email().equals(login_name) &&
+            if(session.getAttribute("classroomID") == null){
+                model.addAttribute("NotAdmin", true);
+                System.out.println(session.getAttribute("classroomID"));
+                return "LoginPage";
+            }
+
+            else if(myMail.get_classroom()!= ((long) session.getAttribute("classroomID"))){
+                model.addAttribute("invalidClassroom", true);
+                return "LoginPage";
+            }
+            else if (myMail.getUser_email().equals(login_name) &&
                     myMail.getUser_password().equals(login_password)&&
-                    myMail.get_classroom()==(id_classroom)){
+                    myMail.get_classroom()==((long) session.getAttribute("classroomID"))){
                 addUserInSession(myMail,session);
                 System.out.println("getting logged in");
+                return "redirect:/userConnected";
             }
-            System.out.println("render user post method");
-            return "redirect:/userConnected";
-        }
-        System.out.println("here I am"); System.out.println(session.getAttribute("login"));
-        System.out.println(session.getAttribute("login_psw"));
-        System.out.println(session.getAttribute("login_first"));
-        System.out.println(session.getAttribute("login_last"));
-        System.out.println(session.getAttribute("is_Admin"));
-        System.out.println(session.getAttribute("classroom"));
-        return "TeacherPage";
+        } return "LoginPage";
     }
+//        System.out.println("here I am"); System.out.println(session.getAttribute("login"));
+//        System.out.println(session.getAttribute("login_psw"));
+//        System.out.println(session.getAttribute("login_first"));
+//        System.out.println(session.getAttribute("login_last"));
+//        System.out.println(session.getAttribute("is_Admin"));
+//        System.out.println(session.getAttribute("classroom"));
+
     private void addUserInSession(User user, HttpSession session){
             session.setAttribute("login", user.getUser_email());
             session.setAttribute("login_psw", user.getUser_password());
@@ -91,18 +96,12 @@ public class LoginController{
     }
     @GetMapping("/adminConnected")
     public String adminPageRender(Model model, HttpSession session) {
-        if((boolean)(session.getAttribute("is_Admin")) == false){
-            System.out.println( "You don't have an admin account");
-            model.addAttribute("NotAdmin", true);
-        }
             System.out.println("My Admin");
-//            model.addAttribute("userIsAdmin",true);
             return "TeacherPage";
         }
     @GetMapping("/userConnected")
         public String userPageRender(Model model) {
             System.out.println("my not admin");
-//            model.addAttribute("userIsNotAdmin",true);
             return "TeacherPage";
         }
 
